b62bb1ed89eb8cbd75f16ffb93256a277e2b0fbc
eduardonk9999 <thiago.novak7@gmail.com>
2021-11-20 12:22:29 -0300

    Add, basico php

---
+++ Basico/index.php
@@ -0,0 +1,20 @@
+<?php 
+
+	// Variaveis
+	$nome = 'Eduardo';
+
+	// Mostrar coisas na tela
+	echo $nome;
+
+	// concatenar
+	echo 'O nome é: '.$nome; 
+
+	// funcoes
+	function getName($nome) {
+		retutn $nome;
+	}
+	getName('Eduardo');
+
+	
+
+?>
---
+++ Basico/opp.php
@@ -0,0 +1,10 @@
+<?php 
+	class Teste{
+		public $nome;
+
+		function __construct($nome) {
+			$this->nome = $nome;
+		}
+	}
+
+?>
